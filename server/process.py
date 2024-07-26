eec6e2293583ab92624cefb0fafa6ea6bde23048
renderer-design <manuelajgampa@gmail.com>
2024-07-26 22:16:52 +0200

    Backend demo setup (express.js and node.js)

---
+++ server/process.py
@@ -0,0 +1,4 @@
+import json
+
+with open('dataInput.json') as file:
+    dataOutput = json.load(file)
---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+//import dataOutput from './dataOutput.json';
+const app = express();
+const spawn = require("child_process").spawn;
+app.use(cors());
+app.use(express.json());
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "hola mundo (⌐■_■)",
+  });
+});
+
+const process = spawn("python", ["./process.py"]);
+
+// el script de python devolverá la salida en el objeto data
+process.stdout.on("data", (data) => {
+  console.log(data);
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await (async () => {}); // completar con una funcion asincrona que se quiera realizar de la api
+
+    res.status(200).send({ api: response.data }); // completar con un objeto parámetro dependiendo de la api
+
+    // Esperar antes de hacer la próxima solicitud
+    await delay(1000); // Espera 1 segundo
+  } catch (error) {
+    console.log(error);
+    alert(error);
+    res.status(500).send({ error });
+  }
+});
+
+app.listen(5000, () =>
+  console.log("El servidor está en https://localhost:5000")
+);
